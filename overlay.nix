e497db36b2906f93b12b3e5d3b71b829b93b1fad
Chris McDonough <chrism@plope.com>
2024-04-01 22:56:38 -0400

    dont use rtcqs as a pkgs overlay

--- overlay.nix
+++ overlay.nix
@@ -66,7 +66,7 @@ with lib;
   linuxPackages_6_6_rt  = recurseIntoAttrs (linuxPackagesFor self.linux_6_6_rt);
   linuxPackages_6_8_rt  = recurseIntoAttrs (linuxPackagesFor self.linux_6_8_rt);
 
-  linuxPackages_rt = self.linuxPackages_6_1_rt;
+  linuxPackages_rt = self.linuxPackages_6_6_rt;
   linux_rt = self.linuxPackages_rt.kernel;
 
   linuxPackages_latest_rt = self.linuxPackages_6_8_rt;
@@ -75,6 +75,5 @@ with lib;
   realtimePatches = callPackage ./pkgs/os-specific/linux/kernel/patches.nix {};
 
   rtirq = callPackage ./pkgs/os-specific/linux/rtirq {};
-  rtcqs = callPackage ./pkgs/rtcqs.nix {};
 
 }
--- pkgs/rtcqs.nix
+++ pkgs/rtcqs.nix
@@ -4,12 +4,14 @@ pkgs.python3.pkgs.buildPythonApplication rec {
     pname = "rtcqs";
     version = "0.6.2";
     format = "pyproject";
+    # Dont check that the gui portion of the rtcqs package can be run; it uses
+    # PySimpleGUI, which, though it is available in nixpkgs, doesn't work
+    # properly; it is also a deliberately obfuscated commercial library, and
+    # thus not worth fixing.  For our purposes, we just want to be able to run
+    # the command-line tool.
+    pythonRuntimeDepsCheckHook = "true";
     buildInputs = [
       pkgs.python3.pkgs.setuptools
-    ];
-    propagatedBuildInputs = [
-      pkgs.python3.pkgs.pysimplegui
-      pkgs.python3.pkgs.rsa
     ];
     src = fetchPypi {
       inherit pname version;
